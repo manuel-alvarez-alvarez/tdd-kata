162ca2f4de1fd39df59a559361b3b2615301bd09
malvarez <manuel.alvarez.alvarez@gmail.com>
2016-12-13 00:19:51 +0100

    Add polymer application for the UI

--- src/main/java/es/manuel/tdd/GameOfLifeMaster.java
+++ src/main/java/es/manuel/tdd/GameOfLifeMaster.java
@@ -1,21 +1,24 @@
 package es.manuel.tdd;
 
 import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.shared.Application;
 import com.netflix.eureka.EurekaServerContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Client app for the TDD-KATA
@@ -25,32 +28,45 @@ import java.util.List;
 public class GameOfLifeMaster {
 
     @Controller
-    @RequestMapping(value = "/gol")
     public class GameOfLifeController {
 
         @Resource
         private EurekaServerContext eurekaServerContext;
 
-        @GetMapping(value = "/service", produces = MediaType.APPLICATION_JSON_VALUE)
+        @Resource
+        private GameOfLife gameOfLife;
+
+        @GetMapping(value = "/service", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
         @ResponseBody
-        public List<InstanceInfo> getServices() {
-            return eurekaServerContext.getRegistry().getApplication("GAME-OF-LIFE-CLIENT").getInstances();
+        public List<String> findServices() {
+            return Optional.ofNullable(eurekaServerContext.getRegistry().getApplication("GAME-OF-LIFE-CLIENT"))
+                    .map(Application::getInstances)
+                    .orElse(Collections.emptyList())
+                    .stream()
+                    .map(InstanceInfo::getInstanceId)
+                    .collect(Collectors.toList());
         }
 
-        @PostMapping(value = "/service/{instanceId}", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
-        public void invokeService(@PathVariable final String instanceId, @RequestBody final String request, final HttpServletResponse response) throws IOException {
-            InstanceInfo instanceInfo = eurekaServerContext.getRegistry()
-                    .getApplication("GAME-OF-LIFE-CLIENT")
-                    .getByInstanceId(new String(Base64.getDecoder().decode(instanceId)));
-            RestTemplate template = new RestTemplate();
-            try {
-                String result = template.postForObject(instanceInfo.getHomePageUrl(), request, String.class);
-                response.setStatus(HttpServletResponse.SC_OK);
-                response.getWriter().write(result);
-            } catch (HttpStatusCodeException e) {
-                response.setStatus(e.getStatusCode().value());
-                response.getWriter().write(e.getMessage());
-            }
+        @PostMapping(value = "/service", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+        @ResponseBody
+        public Map<String, Object> invokeService(@RequestBody final String request) throws IOException {
+            Map<String, Object> result = new LinkedHashMap<>();
+            result.put("", Cell.toWorld(gameOfLife.nextGeneration(Cell.ofWorld(request))));
+            Optional.ofNullable(eurekaServerContext.getRegistry().getApplication("GAME-OF-LIFE-CLIENT")).ifPresent(application ->
+                    application.getInstances().forEach(instanceInfo -> {
+                        RestTemplate template = new RestTemplate();
+                        try {
+                            String generation = template.postForObject(instanceInfo.getHomePageUrl(), request, String.class);
+                            result.put(instanceInfo.getInstanceId(), generation);
+                        } catch (HttpStatusCodeException e) {
+                            Map<String, String> error = new LinkedHashMap<>();
+                            error.put("error", Boolean.TRUE.toString());
+                            error.put("message", e.getMessage());
+                            result.put(instanceInfo.getInstanceId(), error);
+                        }
+                    }));
+
+            return result;
         }
     }
 
