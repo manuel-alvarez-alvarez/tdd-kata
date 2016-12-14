b307e454e2d4a161ba98f39440b71cc1925e1907
malvarez <manuel.alvarez.alvarez@gmail.com>
2016-12-14 04:04:12 +0100

    Continue with the game

--- src/main/java/es/manuel/tdd/GameOfLifeMaster.java
+++ src/main/java/es/manuel/tdd/GameOfLifeMaster.java
@@ -6,19 +6,17 @@ import com.netflix.eureka.EurekaServerContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Client app for the TDD-KATA
@@ -47,26 +45,69 @@ public class GameOfLifeMaster {
                     .collect(Collectors.toList());
         }
 
-        @PostMapping(value = "/service", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+        @PostMapping(value = "/service/{serviceId}", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
         @ResponseBody
-        public Map<String, Object> invokeService(@RequestBody final String request) throws IOException {
-            Map<String, Object> result = new LinkedHashMap<>();
-            result.put("", Cell.toWorld(gameOfLife.nextGeneration(Cell.ofWorld(request))));
-            Optional.ofNullable(eurekaServerContext.getRegistry().getApplication("GAME-OF-LIFE-CLIENT")).ifPresent(application ->
-                    application.getInstances().forEach(instanceInfo -> {
+        public Result invokeService(@RequestBody final String request, @PathVariable final String serviceId, @RequestParam final int frames) throws IOException {
+            List<String> right = IntStream.range(0, frames).collect(
+                    LinkedList::new,
+                    (list, i) -> list.add(Cell.toWorld(gameOfLife.nextGeneration(Cell.ofWorld(list.get(list.size() - 1))))),
+                    LinkedList::addAll);
+            return Optional.ofNullable(eurekaServerContext.getRegistry().getApplication("GAME-OF-LIFE-CLIENT"))
+                    .map(application -> application.getByInstanceId(serviceId))
+                    .map(instanceInfo -> {
                         RestTemplate template = new RestTemplate();
-                        try {
-                            String generation = template.postForObject(instanceInfo.getHomePageUrl(), request, String.class);
-                            result.put(instanceInfo.getInstanceId(), generation);
-                        } catch (HttpStatusCodeException e) {
-                            Map<String, String> error = new LinkedHashMap<>();
-                            error.put("error", Boolean.TRUE.toString());
-                            error.put("message", e.getMessage());
-                            result.put(instanceInfo.getInstanceId(), error);
-                        }
-                    }));
+                        Map<String, Object> variables = new LinkedHashMap<>();
+                        variables.put("frames", frames);
+                        @SuppressWarnings("unchecked")
+                        List<String> computed = template.postForObject(instanceInfo.getHomePageUrl(), request, List.class, variables);
+                        return Result.ok(right, computed);
+                    })
+                    .orElse(Result.fail(HttpStatus.NOT_FOUND.value(), String.format("No service %s found", serviceId)));
+        }
+    }
 
-            return result;
+    public static class Result {
+
+        private final boolean error;
+        private final int statusCode;
+        private final String errorMessage;
+        private final List<String> right;
+        private final List<String> computed;
+
+        public Result(boolean error, int statusCode, String errorMessage, List<String> right, List<String> computed) {
+            this.error = error;
+            this.statusCode = statusCode;
+            this.errorMessage = errorMessage;
+            this.right = right;
+            this.computed = computed;
+        }
+
+        public static Result ok(List<String> right, List<String> computed) {
+            return new Result(false, HttpStatus.OK.value(), null, right, computed);
+        }
+
+        public static Result fail(int statusCode, String errorMessage) {
+            return new Result(true, statusCode, errorMessage, null, null);
+        }
+
+        public boolean isError() {
+            return error;
+        }
+
+        public int getStatusCode() {
+            return statusCode;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public List<String> getRight() {
+            return right;
+        }
+
+        public List<String> getComputed() {
+            return computed;
         }
     }
 
